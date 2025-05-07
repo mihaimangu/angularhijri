7bc136ad88ac443bc9f7485ea8976b915c62234f
Mihai Mangu <mihai.mangu@gmail.com>
2025-05-07 19:08:49 +0300

    hello world

---
+++ myapp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello World</h1>
+  `,
+  styles: [],
+})
+export class AppComponent {}
